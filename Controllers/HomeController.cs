638bf706f050758cbbae2557440264a295c54b19
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-24 13:03:20 +0545

    movie UI edited

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,33 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MovieStoreWeb.Models;
+using MovieStoreWeb.Repositories.Abstract;
 
 namespace MovieStoreWeb.Controllers;
 
 public class HomeController : Controller
 {
-    
+    private readonly IMovieService _movieService;
+
+    public HomeController(IMovieService movieService)
+    {
+        _movieService = movieService;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var movies = _movieService.List();
+        return View(movies);
     }
 
     public IActionResult About()
     {
         return View();
+    }
+    public IActionResult MovieDetail(int movieId)
+    {
+        var movie = _movieService.GetById(movieId);
+        return View(movie);
     }
 
 }
